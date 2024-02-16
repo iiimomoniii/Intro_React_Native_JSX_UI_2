7dc4acb9de3858ab7b3893cba675473242660476
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-16 14:52:18 +0700

    Update
    create sub component inside main component

--- Component1.tsx
+++ Component1.tsx
@@ -1,12 +1,23 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {Component} from 'react'
 
 const Component1 = () => {
   return (
-    <View style={{ flex:1, flexDirection:'column' , backgroundColor:'#FF0000', alignItems:'center', justifyContent:'center'}}>
-      <Text style={{ textAlign:'center', color:'#FFFFFF' }}>WelCome to CodeMobiles</Text>
+    <View style={{ flex:1, flexDirection:'column' , backgroundColor:'#000000', alignItems:'center', justifyContent:'center'}}>
+      {/* First Component */}
+      <Text style={{ textAlign:'center', color:'#FFFFFF', fontSize:20 }}>WelCome to CodeMobiles</Text>
+      {/* Second Component */}
+      <SecondComponent/>
     </View>
   )
+}
+
+class SecondComponent extends Component{
+  render(){
+    return(
+      <Text style={{ textAlign:'center', color:'#FFFFFF', fontSize: 20 }}>WelCome to CodeMobiles 2</Text>
+    );
+  }
 }
 
 export default Component1
