b6c6d3e7870c914baf39e503ad7c9b74986623af
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-16 14:36:19 +0700

    create and use component

---
+++ Component1.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Component1 = () => {
+  return (
+    <View style={{ flex:1, flexDirection:'column' , backgroundColor:'#FF0000', alignItems:'center', justifyContent:'center'}}>
+      <Text style={{ textAlign:'center', color:'#FFFFFF' }}>WelCome to CodeMobiles</Text>
+    </View>
+  )
+}
+
+export default Component1
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import Component1 from './Component1';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Component1);
